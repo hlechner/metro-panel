1f1fb9f959e1742f56a24d84fcc59a35343deb54
hlechner <hlechner@gmail.com>
2020-08-16 03:32:15 -0300

    change default values

--- panelPositions.js
+++ panelPositions.js
@@ -36,14 +36,14 @@ var LEFT = 'LEFT';
 var RIGHT = 'RIGHT';
 
 var defaults = [
+    { element: LEFT_BOX,        visible: true,     position: STACKED_TL },
     { element: SHOW_APPS_BTN,   visible: true,     position: STACKED_TL },
     { element: ACTIVITIES_BTN,  visible: false,    position: STACKED_TL },
-    { element: LEFT_BOX,        visible: true,     position: STACKED_TL },
     { element: TASKBAR,         visible: true,     position: STACKED_TL },
-    { element: CENTER_BOX,      visible: true,     position: STACKED_BR },
     { element: RIGHT_BOX,       visible: true,     position: STACKED_BR },
     { element: DATE_MENU,       visible: true,     position: STACKED_BR },
-    { element: SYSTEM_MENU,     visible: true,     position: STACKED_BR },
+    { element: SYSTEM_MENU,     visible: false,    position: STACKED_BR },
+    { element: CENTER_BOX,      visible: true,     position: STACKED_BR },
     { element: DESKTOP_BTN,     visible: true,     position: STACKED_BR },
 ];
 
